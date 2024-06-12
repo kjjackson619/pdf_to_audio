20b567ecc497cbd66fd8fc32afa6b65a60e74a1a
KJ Jackson <kjjackson619@yahoo.com>
2024-06-12 01:01:43 -0400

    finishing touches

--- pdf_to_audio.py
+++ pdf_to_audio.py
@@ -1,26 +1,26 @@
-import PyPDF2
-import pyttsx3
+from PyPDF2 import PdfReader
+from gtts import gTTS
 
 def pdf_to_text(pdf_path):
     with open(pdf_path, 'rb') as file:
-        reader = PyPDF2.PdfFileReader(file)
+        reader = PdfReader(file)
         text = ""
 
-        for page_num in range(reader.numPages):
-            page = reader.getPage(page_num)
+        for page_number in range(len(reader.pages)): 
+            page = reader.pages[page_number]
             text += page.extract_text()
     return text
 
 def text_to_audio(text, audio_path):
-    engine = pyttsx3.init()
-    engine.save_to_file(text, audio_path)
-    engine.runAndWait()
+   tts = gTTS(text)
+   tts.save(audio_path)
 
 def pdf_to_audio(pdf_path, audio_path):
     text = pdf_to_text(pdf_path)
     text_to_audio(text, audio_path)
 
-pdf_path = ""
-audio_path = ""
+if __name__ == "__main__":
+    pdf_path = input("Enter the path to the PDF file: ")
+    audio_path = input("Enter the path to save the audio file: ")
 
 pdf_to_audio(pdf_path, audio_path)
